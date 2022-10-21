839d602b8d0fb549637addf6da52ccd7a58d3806
Sakthivel <sakthi.samuthiram@gmail.com>
2022-10-21 07:41:01 +0530

    Schema updated to replace length field with durationInSeconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,11 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: () => {},
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
